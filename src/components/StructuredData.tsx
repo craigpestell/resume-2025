ce8885e04eea0b73acb38a47d66a9c7b240b2272
Craig Pestell <craigpestell@gmail.com>
2026-08-27 00:12:51 -0700

    Remove GitHub profile exposure

--- src/components/StructuredData.tsx
+++ src/components/StructuredData.tsx
@@ -21,7 +21,6 @@ export default function StructuredData({ personalInfo }: StructuredDataProps) {
     },
     "sameAs": [
       personalInfo.linkedin,
-      personalInfo.github,
       personalInfo.website
     ],
     "knowsAbout": [
